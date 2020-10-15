32fac7fabfbe157ce59eee82c3009340bfb0b498
chulopapi <marlon.guandique@gmail.com>
2020-10-14 22:04:33 -0700

    deploying again

--- server.js
+++ server.js
@@ -32,6 +32,4 @@ app.listen(PORT, () => {
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
 //   useFindAndModify: false,
 //   useNewUrlParser: true,
-
-//   useUnifiedTopology: true
 // });
